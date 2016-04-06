078e8e36a31224ecafe3cc7555f610ff7a1cf838
babjo <kd980311@naver.com>
2016-04-06 22:27:50 +0900

    프로젝트2 완성

--- image_transition/index.js
+++ image_transition/index.js
@@ -4,84 +4,74 @@ $(function(){
 		$(e).click(function(){
 			var img = $(e).children().eq(0);
 			var title = $(e).children().eq(1);
+			var titleHeight = title.height();
 
-			//$('.detail-page-layer').css('display','block');
-			bindMoveEvent2(title, 10);
-			bindMoveEvent(img, 40);
+			bindMoveEvent(title,
+				function(copy){
+					return {top : parseInt(copy.css('top'), 10), left : parseInt(copy.css('left'), 10), fontSize : parseInt(copy.css('font-size'), 10), fontWeight : parseInt(copy.css('font-weight'), 10)};
+				},
+				function(copy){
+					var fontWeight = 500;
+					var fontSize = 20;
+					var screenTop = 10;
+
+					var offset = copy.offset();
+					var windowWidth = $(window).width();
+					var scrollTop = $("body").scrollTop();
+					var contentWidth = copy.width();
+					var contentTop = parseInt(copy.css('top'), 10);
+
+					var left = (windowWidth - contentWidth)/2;
+					var top = -offset.top + contentTop + screenTop + scrollTop;
+					copy.css({top: top+"px", left: left+"px", fontSize: fontSize+"px", fontWeight : fontWeight});					
+				},
+				function(copy, initialCss){
+					copy.css({top:initialCss.top+'px', left:initialCss.left+'px', "font-size" : initialCss.fontSize+"px", "font-weight": initialCss.fontWeight});
+				});
+			
+			bindMoveEvent(img, 
+				function(copy){
+					return {top :  parseInt(copy.css('top'), 10), left : parseInt(copy.css('left'), 10)};
+				},
+				function(copy){
+					var screenTop = 40;
+					
+					var offset = copy.offset();
+					var windowWidth = $(window).width();
+					var scrollTop = $("body").scrollTop();
+					var contentWidth = copy.width();
+					var contentTop = parseInt(copy.css('top'), 10);
+
+					var left = (windowWidth - contentWidth)/2;
+					var top = -offset.top + contentTop + screenTop + scrollTop;
+					copy.css({top: top+"px", left: left+"px"});
+				},
+				function(copy, initialCss){
+					//console.log(titleHeight);
+					copy.css({top: initialCss.top - titleHeight +'px', left: initialCss.left+'px'});
+				});
 		});
 	});
 
-	function bindMoveEvent(e, topValue){
+	function bindMoveEvent(e, saveInitial, change ,loadInitial){
 		$("body").css('overflow', 'hidden');
 
-			var copy = copyToLayerAndShowLayer(e);
-			var initLeft = parseInt(copy.css('left'), 10);
-			var initTop = parseInt(copy.css('top'), 10);
+		var copy = copyToLayerAndShowLayer(e);
+		var init = saveInitial(copy);
 
-			move(copy, topValue);
-
-			$('.detail-page-layer').click(function(){
-				copy.css({top:initTop+'px', left:initLeft+'px'});
-				var layer = $(this);
-				copy.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
-					function(e) {
-						layer.children().first().empty();
-						layer.css('display', 'none');
-						layer.click(null);
-						$("body").css('overflow', 'auto');
-					});
-			});
-	}
-
-	function bindMoveEvent2(e, topValue){
-		$("body").css('overflow', 'hidden');
-
-			var copy = copyToLayerAndShowLayer(e);
-			var fontSize = parseInt(copy.css('font-size'), 10);
-			console.log(fontSize);
-			var fontWeight = parseInt(copy.css('font-weight'), 10);
-			console.log(fontWeight);
-			var initLeft = parseInt(copy.css('left'), 10);
-			var initTop = parseInt(copy.css('top'), 10);
-
-			move2(copy, topValue, 20, 500);
-
-			$('.detail-page-layer').click(function(){
-				copy.css({top:initTop+'px', left:initLeft+'px'});
-				var layer = $(this);
-				copy.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
-					function(e) {
-						layer.children().first().empty();
-						layer.css('display', 'none');
-						layer.click(null);
-						$("body").css('overflow', 'auto');
-					});
-			});
-	}
-
-	function move2(jqueryElement, topValue, fontSize, fontWeight){
-		var offset = jqueryElement.offset();
-		var windowWidth = $(window).width();
-		var scrollTop = $("body").scrollTop();
-		var contentWidth = jqueryElement.width();
-		var contentTop = parseInt(jqueryElement.css('top'), 10);
-
-		var left = (windowWidth - contentWidth)/2;
-		var top = -offset.top + contentTop + topValue + scrollTop;
-		jqueryElement.css({top: top+"px", left: left+"px", "font-size" : fontSize+"px", "font-weight" : fontWeight});
-	}
-
-
-	function move(jqueryElement, topValue){
-		var offset = jqueryElement.offset();
-		var windowWidth = $(window).width();
-		var scrollTop = $("body").scrollTop();
-		var contentWidth = jqueryElement.width();
-		var contentTop = parseInt(jqueryElement.css('top'), 10);
-
-		var left = (windowWidth - contentWidth)/2;
-		var top = -offset.top + contentTop + topValue + scrollTop;
-		jqueryElement.css({top: top+"px", left: left+"px"});
+		change(copy);
+		
+		$('.detail-page-layer').click(function(){
+			loadInitial(copy, init);		
+			var layer = $(this);
+			copy.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+				function(e) {
+					layer.children().first().empty();
+					layer.css('display', 'none');
+					layer.click(null);
+					$("body").css('overflow', 'auto');
+				});
+		});
 	}
 
 	function copyToLayerAndShowLayer(e){
